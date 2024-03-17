bf88ba3a6458e89fdb1f6ef66ff7bfe4a0e52a78
namratraj29 <namrataraj2926@.com>
2024-03-17 11:31:06 +0530

    added

---
+++ app.js
@@ -0,0 +1,13 @@
+//object literals
+let student={
+    name:"namrata raj",
+    age:21,
+    marks:8.32
+}
+console.log(student)
+let item={
+    price:100.99,
+    discount:50,
+    colors:["red","pink"]
+}
+console.log(item)
