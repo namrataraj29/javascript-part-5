69948a44cfe639834ebc7a6117fb0838fb4509b9
namratraj29 <namrataraj2926@.com>
2024-03-17 21:50:51 +0530

    added

--- app.js
+++ app.js
@@ -26,4 +26,70 @@ console.log(post["likes"])
 console.log(post.content)
 console.log(post.tags)
 console.log(post.tags[0])
-console.log(post.prop[])
+
+//get values
+
+abj={
+    1:"a",
+    2:"b",
+    3:"c",
+    4:"d"
+}
+console.log(abj)
+
+//change or update on object
+
+ let stu = {
+    name:"namrata raj",
+    class:"bca",
+    city:"bihar",
+    grade:"A"
+}
+console.log(stu)
+console.log(stu.name="milky")
+console.log(stu.class="mca")
+console.log(stu.city="bihar")
+console.log(stu.grade="B+")
+console.log(stu.gender="female")
+console.log(stu)
+
+//for deletion
+delete stu.gender
+console.log(stu)
+
+//nested object
+
+let classinfo={
+    namrata:{
+        class:"bca",
+        city:"bihar"
+    },
+    milky: {
+        class:"mca",
+        city:"patna"
+    },
+    raj:{
+        class:"mba",
+        city:"delhi"
+    }
+}
+console.log(classinfo)
+console.log(classinfo.namrata)
+
+//array of objects
+let info= [ {
+    namrata:{
+        class:"bca",
+        city:"bihar"
+    },
+    milky: {
+        class:"mca",
+        city:"patna"
+    },
+    raj:{
+        class:"mba",
+        city:"delhi"
+    }
+}
+]
+console.log(info)
